307c4cf836ee0038bfc5596cc4869a74491aa77c
Thanos <111999343+Sachaa-Thanasius@users.noreply.github.com>
2024-06-28 21:00:56 -0400

    Remove the super class for `str` entirely.

--- stdlib/builtins.pyi
+++ stdlib/builtins.pyi
@@ -431,7 +431,7 @@ class _FormatMapMapping(Protocol):
 class _TranslateTable(Protocol):
     def __getitem__(self, key: int, /) -> str | int | None: ...
 
-class str(Collection[str]):
+class str:
     @overload
     def __new__(cls, object: object = ...) -> Self: ...
     @overload
